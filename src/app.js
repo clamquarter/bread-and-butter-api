b63c4fc8680f956a2391a7e53cb9e439d372af46
India Walker <indiadoria@gmail.com>
2020-12-16 11:31:06 -0500

    updated dummy database

--- src/app.js
+++ src/app.js
@@ -18,15 +18,18 @@ const USERS = [
 ]
 
 const RECIPES = [
-  {title: 'Tomato Bisque Soup',
+  {id: 0,
+    title: 'Tomato Bisque Soup',
   ingredients: ['large onion', '4 cloves of garlic', '2 medium shallots', '4 tbsp each of olive oil and butter', '36 oz can of peeled tomatos', '36 oz can of tomato sauce', '2 cups heavy cream', '4 cups chicken broth'],
   instructions: ['Saute veggies until fragrant, around 5 minutes.', 'Add peeled tomatoes, chicken broth, and basil.', 'Bring everything to a boil, then let it all simmer for 15 minutes.', 'Use an immersion blender to smooth the soup.', 'Add the cream and tomato sauce, stirring to combine.', 'Eat some soup!']
 },
-{title: 'Egg in a Basket',
+{id: 1,
+  title: 'Egg in a Basket',
   ingredients: ['2 eggs', '2 thick slices of bread', '2 tbsp butter', 'seasonings to taste'],
   instructions: ['Melt butter in a large skillet.', 'Meanwhile, cut a circular hole in each slice of bread.', 'Add holed bread to the skillet, making sure to get butter on both sides.', 'Carefully crack an egg into each hole and let fry for 2-3 minutes.', 'CAREFULLY flip the bread slices, and allow the egg to cook for about 30 seconds more.', 'Eat!']
 },
-{title: 'Dummy Recipe',
+{id: 2,
+  title: 'Dummy Recipe',
   ingredients: ['bananas', 'chocolate', 'ice cream'],
   instructions: ['Eat the bananas.', 'Eat the chocolate.', 'Eat the ice cream.', 'Go to the gym!']
 }
